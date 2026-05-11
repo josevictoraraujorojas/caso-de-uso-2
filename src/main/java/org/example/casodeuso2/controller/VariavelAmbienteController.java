a9cd69cdc640a40036a4d183d7a09ac7e80feb1f
josevictoraraujorojas <kaneki1212112121@gmail.com>
2026-05-11 15:10:50 -0300

    Adicionadas respostas HTTP, endpoint e put de edição para variavel ambiente

--- src/main/java/org/example/casodeuso2/controller/VariavelAmbienteController.java
+++ src/main/java/org/example/casodeuso2/controller/VariavelAmbienteController.java
@@ -4,6 +4,8 @@ import org.example.casodeuso2.dto.VariavelAmbienteCreateDTO;
 import org.example.casodeuso2.dto.VariavelAmbienteResponseDTO;
 import org.example.casodeuso2.service.VariavelAmbienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,45 +13,90 @@ import java.util.List;
 @RestController
 @RequestMapping("/variavelAmbiente")
 public class VariavelAmbienteController {
+
     private final VariavelAmbienteService service;
 
     @Autowired
-    public VariavelAmbienteController(VariavelAmbienteService service) {
+    public VariavelAmbienteController(
+            VariavelAmbienteService service) {
+
         this.service = service;
     }
 
     // POST
     @PostMapping
-    public VariavelAmbienteResponseDTO criar(@RequestBody VariavelAmbienteCreateDTO variavelAmbienteCreateDTO) {
-        return service.salvar(variavelAmbienteCreateDTO);
+    public ResponseEntity<VariavelAmbienteResponseDTO> criar(
+            @RequestBody VariavelAmbienteCreateDTO dto) {
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.salvar(dto));
     }
 
-    //PUT
+    // PUT - EDITAR
+    @PutMapping("/{id}")
+    public ResponseEntity<VariavelAmbienteResponseDTO> editar(
+            @PathVariable Long id,
+            @RequestBody VariavelAmbienteCreateDTO dto) {
+
+        return ResponseEntity.ok(
+                service.editar(id, dto)
+        );
+    }
+
+    // PUT - ADICIONAR LIMITE AMBIENTAL
     @PutMapping("/{variavelAmbienteId}/limiteAmbiental/{limiteId}")
-    public VariavelAmbienteResponseDTO adicionarSensor(@PathVariable Long variavelAmbienteId, @PathVariable Long limiteId) {
-        return service.adicionarLimiteAmbiental(variavelAmbienteId,limiteId);
+    public ResponseEntity<VariavelAmbienteResponseDTO> adicionarSensor(
+            @PathVariable Long variavelAmbienteId,
+            @PathVariable Long limiteId) {
+
+        return ResponseEntity.ok(
+                service.adicionarLimiteAmbiental(
+                        variavelAmbienteId,
+                        limiteId
+                )
+        );
     }
 
-    // GET todos
+    // GET TODOS
     @GetMapping
-    public List<VariavelAmbienteResponseDTO> listar() {
-        return service.listar();
+    public ResponseEntity<List<VariavelAmbienteResponseDTO>> listar() {
+
+        return ResponseEntity.ok(
+                service.listar()
+        );
     }
 
-    // GET por ID
+    // GET POR ID
     @GetMapping("/{id}")
-    public VariavelAmbienteResponseDTO buscarPorId(@PathVariable Long id) {
-        return service.buscarPorId(id);
+    public ResponseEntity<VariavelAmbienteResponseDTO> buscarPorId(
+            @PathVariable Long id) {
+
+        return ResponseEntity.ok(
+                service.buscarPorId(id)
+        );
     }
 
     // DELETE
     @DeleteMapping("/{id}")
-    public void deletar(@PathVariable Long id) {
+    public ResponseEntity<Void> deletar(
+            @PathVariable Long id) {
+
         service.deletar(id);
+
+        return ResponseEntity.noContent().build();
     }
 
+    // DELETE - REMOVER LIMITE AMBIENTAL
     @DeleteMapping("/{variavelAmbienteId}/limiteAmbiental/{limiteId}")
-    public VariavelAmbienteResponseDTO removerSensor(@PathVariable Long variavelAmbienteId, @PathVariable Long limiteId) {
-        return service.removerLimiteAmbiental(variavelAmbienteId,limiteId);
+    public ResponseEntity<VariavelAmbienteResponseDTO> removerSensor(
+            @PathVariable Long variavelAmbienteId,
+            @PathVariable Long limiteId) {
+
+        return ResponseEntity.ok(
+                service.removerLimiteAmbiental(
+                        variavelAmbienteId,
+                        limiteId
+                )
+        );
     }
 }
--- src/main/java/org/example/casodeuso2/service/VariavelAmbienteService.java
+++ src/main/java/org/example/casodeuso2/service/VariavelAmbienteService.java
@@ -1,6 +1,5 @@
 package org.example.casodeuso2.service;
 
-
 import org.example.casodeuso2.dto.VariavelAmbienteCreateDTO;
 import org.example.casodeuso2.dto.VariavelAmbienteResponseDTO;
 import org.example.casodeuso2.model.LimiteAmbiental;
@@ -9,65 +8,198 @@ import org.example.casodeuso2.repository.LimiteAmbientalRepository;
 import org.example.casodeuso2.repository.VariavelAmbienteRepository;
 import org.example.casodeuso2.util.DataMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Objects;
 
 @Service
 public class VariavelAmbienteService {
+
     private final VariavelAmbienteRepository repository;
     private final LimiteAmbientalRepository limiteAmbientalRepository;
 
     @Autowired
-    public VariavelAmbienteService(VariavelAmbienteRepository repository, LimiteAmbientalRepository limiteAmbientalRepository) {
+    public VariavelAmbienteService(
+            VariavelAmbienteRepository repository,
+            LimiteAmbientalRepository limiteAmbientalRepository) {
+
         this.repository = repository;
         this.limiteAmbientalRepository = limiteAmbientalRepository;
     }
 
-    // salvar
-    public VariavelAmbienteResponseDTO salvar(VariavelAmbienteCreateDTO variavelAmbienteCreateDTO) {
-        return DataMapper.parseObject(repository.save(DataMapper.parseObject(variavelAmbienteCreateDTO,VariavelAmbiente.class)),VariavelAmbienteResponseDTO.class);
+    // SALVAR
+    public VariavelAmbienteResponseDTO salvar(
+            VariavelAmbienteCreateDTO dto) {
+
+        if (dto == null) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    "Dados da variável ambiente inválidos"
+            );
+        }
+
+        VariavelAmbiente variavelAmbiente =
+                DataMapper.parseObject(dto, VariavelAmbiente.class);
+
+        return DataMapper.parseObject(
+                repository.save(variavelAmbiente),
+                VariavelAmbienteResponseDTO.class
+        );
     }
 
-    public VariavelAmbienteResponseDTO adicionarLimiteAmbiental(Long variavelAmbientalId,Long limiteAmbientalId) {
-        VariavelAmbiente variavelAmbiente = repository.findById(variavelAmbientalId).orElseThrow(() -> new RuntimeException("Variavel de Ambiente não encontrada"));
-        LimiteAmbiental limiteAmbiental = limiteAmbientalRepository.findById(limiteAmbientalId).orElseThrow(() -> new RuntimeException("Limite Ambiental nâo encontrado"));
-        if (variavelAmbiente.getLimite()==null){
-            variavelAmbiente.setLimite(limiteAmbiental);
-            return DataMapper.parseObject(repository.save(variavelAmbiente),VariavelAmbienteResponseDTO.class);
+    // EDITAR
+    public VariavelAmbienteResponseDTO editar(
+            Long variavelAmbienteId,
+            VariavelAmbienteCreateDTO dto) {
+
+        VariavelAmbiente variavelAmbiente =
+                repository.findById(variavelAmbienteId)
+                        .orElseThrow(() -> new ResponseStatusException(
+                                HttpStatus.NOT_FOUND,
+                                "Variável ambiente não encontrada"
+                        ));
+
+        variavelAmbiente.setNome(dto.getNome());
+        variavelAmbiente.setUnidade(dto.getUnidade());
+        variavelAmbiente.setDescricao(dto.getDescricao());
+
+        return DataMapper.parseObject(
+                repository.save(variavelAmbiente),
+                VariavelAmbienteResponseDTO.class
+        );
+    }
+
+    // ADICIONAR LIMITE AMBIENTAL
+    public VariavelAmbienteResponseDTO adicionarLimiteAmbiental(
+            Long variavelAmbientalId,
+            Long limiteAmbientalId) {
+
+        VariavelAmbiente variavelAmbiente =
+                repository.findById(variavelAmbientalId)
+                        .orElseThrow(() -> new ResponseStatusException(
+                                HttpStatus.NOT_FOUND,
+                                "Variável de ambiente não encontrada"
+                        ));
+
+        LimiteAmbiental limiteAmbiental =
+                limiteAmbientalRepository.findById(limiteAmbientalId)
+                        .orElseThrow(() -> new ResponseStatusException(
+                                HttpStatus.NOT_FOUND,
+                                "Limite ambiental não encontrado"
+                        ));
+
+        if (variavelAmbiente.getLimite() != null &&
+                Objects.equals(
+                        variavelAmbiente.getLimite().getId(),
+                        limiteAmbiental.getId())) {
+
+            throw new ResponseStatusException(
+                    HttpStatus.CONFLICT,
+                    "Esse limite já está vinculado a esta variável ambiente"
+            );
         }
-        if (Objects.equals(variavelAmbiente.getLimite().getId(), limiteAmbiental.getId())) {
-            throw new RuntimeException("Esse Limite já esta vinculada com esta Variavel de Ambiente");
-        }
+
         variavelAmbiente.setLimite(limiteAmbiental);
-        return DataMapper.parseObject(repository.save(variavelAmbiente),VariavelAmbienteResponseDTO.class);
+
+        return DataMapper.parseObject(
+                repository.save(variavelAmbiente),
+                VariavelAmbienteResponseDTO.class
+        );
     }
 
-    // listar todos
+    // LISTAR TODOS
     public List<VariavelAmbienteResponseDTO> listar() {
-        return DataMapper.parseListObjects(repository.findAll(),VariavelAmbienteResponseDTO.class);
+
+        List<VariavelAmbiente> variaveis =
+                repository.findAll();
+
+        if (variaveis.isEmpty()) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    "Nenhuma variável ambiente encontrada"
+            );
+        }
+
+        return DataMapper.parseListObjects(
+                variaveis,
+                VariavelAmbienteResponseDTO.class
+        );
     }
-    // buscar por id
+
+    // BUSCAR POR ID
     public VariavelAmbienteResponseDTO buscarPorId(Long id) {
-        return DataMapper.parseObject(repository.findById(id).orElseThrow(() -> new RuntimeException("Variavel ambiente não encontrado")),VariavelAmbienteResponseDTO.class);
+
+        VariavelAmbiente variavelAmbiente =
+                repository.findById(id)
+                        .orElseThrow(() -> new ResponseStatusException(
+                                HttpStatus.NOT_FOUND,
+                                "Variável ambiente não encontrada"
+                        ));
+
+        return DataMapper.parseObject(
+                variavelAmbiente,
+                VariavelAmbienteResponseDTO.class
+        );
     }
 
-    // deletar
+    // DELETAR
     public void deletar(Long id) {
-        repository.deleteById(id);
+
+        VariavelAmbiente variavelAmbiente =
+                repository.findById(id)
+                        .orElseThrow(() -> new ResponseStatusException(
+                                HttpStatus.NOT_FOUND,
+                                "Variável ambiente não encontrada"
+                        ));
+
+        repository.delete(variavelAmbiente);
     }
 
-    public VariavelAmbienteResponseDTO removerLimiteAmbiental(Long variavelAmbientalId,Long limiteAmbientalId) {
-        VariavelAmbiente variavelAmbiente = repository.findById(variavelAmbientalId).orElseThrow(() -> new RuntimeException("Variavel de Ambiente não encontrada"));
-        LimiteAmbiental limiteAmbiental = limiteAmbientalRepository.findById(limiteAmbientalId).orElseThrow(() -> new RuntimeException("Limite Ambiental nâo encontrado"));
-        if (variavelAmbiente.getLimite()==null){
-            throw new RuntimeException("Essa Variavel ambiental não possui Limite ambiental");
+    // REMOVER LIMITE AMBIENTAL
+    public VariavelAmbienteResponseDTO removerLimiteAmbiental(
+            Long variavelAmbientalId,
+            Long limiteAmbientalId) {
+
+        VariavelAmbiente variavelAmbiente =
+                repository.findById(variavelAmbientalId)
+                        .orElseThrow(() -> new ResponseStatusException(
+                                HttpStatus.NOT_FOUND,
+                                "Variável ambiente não encontrada"
+                        ));
+
+        LimiteAmbiental limiteAmbiental =
+                limiteAmbientalRepository.findById(limiteAmbientalId)
+                        .orElseThrow(() -> new ResponseStatusException(
+                                HttpStatus.NOT_FOUND,
+                                "Limite ambiental não encontrado"
+                        ));
+
+        if (variavelAmbiente.getLimite() == null) {
+
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    "Essa variável ambiente não possui limite ambiental"
+            );
         }
-        if (!Objects.equals(variavelAmbiente.getLimite().getId(), limiteAmbiental.getId())) {
-            throw new RuntimeException("Esse Limite não esta vinculada com esta Variavel de Ambiente");
+
+        if (!Objects.equals(
+                variavelAmbiente.getLimite().getId(),
+                limiteAmbiental.getId())) {
+
+            throw new ResponseStatusException(
+                    HttpStatus.CONFLICT,
+                    "Esse limite não está vinculado a esta variável ambiente"
+            );
         }
+
         variavelAmbiente.setLimite(null);
-        return DataMapper.parseObject(repository.save(variavelAmbiente),VariavelAmbienteResponseDTO.class);
+
+        return DataMapper.parseObject(
+                repository.save(variavelAmbiente),
+                VariavelAmbienteResponseDTO.class
+        );
     }
 }
